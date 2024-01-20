81611d95c33fc8c6b726ae801c5e359df87cef87
AngelArabadzhiev08 <90851649+AngelArbadzhiev@users.noreply.github.com>
2024-01-20 11:06:42 +0200

    Add files via upload

--- CircularQueue/CircularQueue/Program.cs
+++ CircularQueue/CircularQueue/Program.cs
@@ -4,55 +4,55 @@
     {
         static void Main(string[] args)
         {
-            CircularQueue<int> queue = new CircularQueue<int>();
-            queue.Enqueue(1);
-            queue.Enqueue(2);
-            queue.Enqueue(3);
-            queue.Enqueue(4);
-            var arrqueue = queue.ToArray();
-            for (int i = 0; i < queue.Count; i++)
+            CircularQueue<int> queue = new CircularQueue<int>(); //creating a new circular queue
+            queue.Enqueue(1);//inserting elements
+            queue.Enqueue(2);//inserting elements
+            queue.Enqueue(3);//inserting elements
+            queue.Enqueue(4);//inserting elements
+            var arrqueue = queue.ToArray(); //casting queue to an arr
+            for (int i = 0; i < queue.Count; i++) // looping to the length of the queue and then printing each element
             {
                 Console.Write(arrqueue[i] + " ");
             }
-            queue.Dequeue();
+            queue.Dequeue(); // removing the last element, because it is on FILO principle
             Console.WriteLine();
-            for (int i = 0; i < queue.Count; i++)
+            for (int i = 0; i < queue.Count; i++) // displaying the new arr with the last num removed
             {
                 Console.Write(arrqueue[i] + " ");
             }
 
         }
     }
-    public class CircularQueue<T>
+    public class CircularQueue<T> //creating a generic class
     {
-        private T[] elements;
+        private T[] elements;//creating an arr of elements
         private int startIndex = 0;
         private int endIndex = 0;
 
-        private const int DefaultCapacity = 4;
-        public int Count { get; private set; }
-        public CircularQueue()
+        private const int DefaultCapacity = 4; //setting the default capacity to 4
+        public int Count { get; private set; } 
+        public CircularQueue() //ctor to initialize if not entered a capacity it sets to 16
         {
             int capacity = 16;
             elements = new T[capacity];
         }
-        public CircularQueue(int capacity = DefaultCapacity)
+        public CircularQueue(int capacity = DefaultCapacity) // if entered a capacity set to that elase above
         {
             elements = new T[capacity];
         }
-        public void Enqueue(T element)
+        public void Enqueue(T element) //inserting an element into the queue (FILO prinicple)
         {
             if (this.Count >= this.elements.Length)
             {
-                this.Grow();
+                this.Grow(); //growing the queue if the nums are more than it's capacity
             }
             this.elements[this.endIndex] = element;
-            this.endIndex = (this.endIndex + 1) % this.elements.Length;
+            this.endIndex = (this.endIndex + 1) % this.elements.Length; //math formula (given)
             this.Count++;
         }
         public void Grow()
         {
-            var newElements = new T[2 * this.elements.Length];
+            var newElements = new T[2 * this.elements.Length]; //creating a new array that is double the capacity of the previus one 
             this.CopyAllElementsTo(newElements);
             this.elements = newElements;
             this.startIndex = 0;
@@ -65,7 +65,7 @@
             for (int i = 0; i < this.Count; i++)
             {
                 resultArr[destinationIndex] = this.elements[sourceindex];
-                sourceindex = (sourceindex + 1 ) % this.elements.Length;
+                sourceindex = (sourceindex + 1 ) % this.elements.Length; //math formula (given)
                 destinationIndex++;
 
             }
@@ -77,7 +77,7 @@
                 throw new InvalidOperationException();
             }
             var result = this.elements[startIndex];
-            this.startIndex = (startIndex + 1) % this.elements.Length;
+            this.startIndex = (startIndex + 1) % this.elements.Length; //math formula (given)
             this.Count--;
             return result;
         }
